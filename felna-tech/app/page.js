71995113ddd633de6c36eabc285bffd64a1a2d50
Samsuj-joha <samsujjoha99@gmail.com>
2023-10-26 15:25:33 +0600

    new brances included

--- felna-tech/app/page.js
+++ felna-tech/app/page.js
@@ -7,6 +7,9 @@ const Main = () => {
       <h1>this is the header page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, tempora?</p>
     </header>
+    <main>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, quod!</p>
+    </main>
 
       
       
