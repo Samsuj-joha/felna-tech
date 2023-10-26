401f1b9a55d03673f8e7910b82217ae5bfb82d5f
Samsuj-joha <samsujjoha99@gmail.com>
2023-10-26 11:17:39 +0600

    added the changes file

---
+++ felna-tech/app/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+    <header>
+      <h1>this is the header page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, tempora?</p>
+    </header>
+    <section>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, animi!</p>
+    </section>
+      
+      
+    </>
+  )
+}
+
+export default Main 
